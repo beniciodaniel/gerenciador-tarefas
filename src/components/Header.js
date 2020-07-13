71bf396df702dd6012d3760a39cd2d8ca1a5eaab
Benicio Daniel <beni@MacBook-Pro-de-Benicio.local>
2020-07-12 21:59:06 -0300

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='header'>
-      <h1>Gerenciador de Tarefas </h1>
+      <h1>Gerenciador de Tarefas</h1>
     </div>
   )
 }
